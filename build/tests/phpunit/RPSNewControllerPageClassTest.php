d56fc09a7226f9743502a5202f7e30278a15553c
Dave <damanshia@ebay.com>
2013-01-02 09:10:46 +0000

    test

--- build/tests/phpunit/RPSNewControllerPageClassTest.php
+++ build/tests/phpunit/RPSNewControllerPageClassTest.php
@@ -12,4 +12,34 @@ class RPSNewControllerPageClassTest extends PHPUnit_Framework_TestCase {
         $this->assertTrue(is_object($page));
     }
 
+    public function testExecuteReturnsAValue() {
+        $control = new \Controller\Page();
+        $mockPageVars = array();
+        $mockPageVars["route"] = array();
+        $mockPageVars["route"]["action"] = "hi";
+        $returnRay = $control->execute($mockPageVars);
+        $this->assertTrue( !is_null($returnRay) );
+    }
+
+    public function testExecuteReturnsAnArray() {
+        $mockPageVars = array();
+        $mockPageVars["route"] = array();
+        $mockPageVars["route"]["action"] = "hi";
+        $control = new \Controller\Page();
+        $returnRay = $control->execute($mockPageVars);
+        $this->assertTrue( is_array($returnRay) );
+    }
+
+    public function testExecuteReturnsAnArrayWithStandardPageViewWhenActionIsHi() {
+        $mockPageVars = array();
+        $mockPageVars["route"] = array();
+        $mockPageVars["route"]["action"] = "hi";
+        $control = new \Controller\Page();
+        $returnRay = $control->execute($mockPageVars);
+        $this->assertTrue( array_key_exists("type", $returnRay) );
+        $this->assertTrue( $returnRay["type"]=="view" );
+        $this->assertTrue( array_key_exists("view", $returnRay) );
+        $this->assertTrue( $returnRay["view"]=="standardPage" );
+    }
+
 }
